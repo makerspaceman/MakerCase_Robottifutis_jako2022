4220fe48c185dbc8976eb3530a6032c075004cc4
makerspaceman <91957914+makerspaceman@users.noreply.github.com>
2022-06-04 14:10:32 +0300

    Päivitys pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,69 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 50)
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Reverse, 50)
+        basic.pause(50)
+        Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor1)
+        Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor2)
+    } else if (receivedNumber == 2) {
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 50)
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Forward, 50)
+    } else if (receivedNumber == 3) {
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, 50)
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Reverse, 50)
+    } else if (receivedNumber == 4) {
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, 50)
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Forward, 50)
+    } else if (receivedNumber == 5) {
+        Kitronik_Robotics_Board.allOff()
+    }
+})
 radio.setGroup(35)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            . . . . .
+            . . . . .
+            `)
+        radio.sendNumber(1)
+    } else if (input.buttonIsPressed(Button.B)) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # . . . .
+            . # . . .
+            . . # . .
+            `)
+        radio.sendNumber(2)
+    } else if (input.buttonIsPressed(Button.AB)) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            . . . . #
+            . . . # .
+            . . # . .
+            `)
+        radio.sendNumber(3)
+    } else if (input.logoIsPressed()) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+        radio.sendNumber(4)
+    } else if (input.isGesture(Gesture.Shake)) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        radio.sendNumber(5)
+    }
+})
